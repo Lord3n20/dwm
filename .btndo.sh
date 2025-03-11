13c2a5443aff141cb6d38f42bf1334d4cad6d78e
Lord3n20 <enzo@brandsch.com>
2025-03-11 12:33:27 +0100

    geändert:       .btndo.sh

--- .btndo.sh
+++ .btndo.sh
@@ -1,20 +1,27 @@
 if [ $1 = "vol" ]; then
-    echo Ändere Lautstärke
-    if [ $2 = "up" ]; then
-        echo Lauter
-    elif [ $2 = "down" ]; then
-        echo leiser
-    elif [ $2 = "mute" ]; then
-        echo stumm
-    fi
+  if [ $2 = "up" ]; then
+    pulsemixer --change-volume +5
+  elif [ $2 = "down" ]; then
+    pulsemixer --change-volume -5
+  elif [ $2 = "mute" ]; then
+    pactl set-sink-mute @DEFAULT_SINK@ toggle
+  fi
 elif [ $1 = "light" ]; then
-    echo Ändere Lautstärke
-    if [ $2 = "up" ]; then
-        sudo light -A 10
-    elif [ $2 = "down" ]; then
-        sudo light -U 10
-    fi
-elif [ $1 = "thinkvantage" ]; then
-    st -e bash -c "echo wixer && sleep 1"
-    #mpv ~/.bndo.m4a
+  if [ $2 = "down" ]; then
+    sudo light -U 5
+  elif [ $2 = "up" ]; then
+    sudo light -A 10
+  fi
+elif [ $1 = "screen" ]; then
+  flameshot gui
+elif [ $1 = "reloadbg" ]; then
+  feh --no-fehbg --bg-fill --randomize ~/.wall/*
+elif [ $1 = "playback" ]; then
+  if [[ $2 = "toggle" ]]; then
+    playerctl play-pause
+  elif [[ $2 = "again" ]]; then
+    playerctl previous
+  elif [[ $2 = "skip" ]]; then
+    playerctl next
+  fi
 fi
