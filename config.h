996b9765d7f8b54c427bf0fae9759f7fc2211e37
3n20 <enzo@brandsch.com>
2025-05-18 18:08:42 +0200

    changed config

--- config.h
+++ config.h
@@ -32,7 +32,7 @@ static const unsigned int alphas[][3]      = {
 
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", "", "6", "7", "8" };
+static const char *tags[] = { "", "", "", "", "5"};
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -40,10 +40,9 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class                        instance    title       tags mask     isfloating   monitor */
-	{ "zen",  NULL,       NULL,       1 << 1,       0,           -1 },
-	{ "discord",                    NULL,       NULL,       1 << 2,       0,           -1 },
-	{ "betterbird",                 NULL,       NULL,       1 << 3,       0,           -1 },
-	{ "steam",                      NULL,       NULL,       1 << 4,       0,           -1 },
+	{ "zen",        NULL,       NULL,       1 << 1,       0,           -1 },
+  { "discord",    NULL,       NULL,       1 << 2,       0,           -1 },
+  { "betterbird", NULL,       NULL,       1 << 3,       0,           -1 }
 };
 
 /* layout(s) */
